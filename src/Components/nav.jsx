4dfb0c19e9af3a63970b1290564040c27623d3fb
Luis Manzur <luismanzur91@outlook.com>
2022-03-12 23:55:44 -0400

    log out

--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -3,12 +3,21 @@
 import { useAuth } from '../context/authContext';
 
 // Router
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 
 
 
 export default function Nav() {
+
+  const {logout} = useAuth()
+  const navigate = useNavigate()
+
+  const handleLogout = async () => {
+    await logout()
+    navigate("/login")
+  }
+
   return (
-    <><Link to="/">Home</Link><Link to="/movies">Peliculas</Link><button>Log out</button></>
+    <><Link to="/">Home</Link><Link to="/movies">Peliculas</Link><button onClick={handleLogout}>Log out</button></>
   );
 }
