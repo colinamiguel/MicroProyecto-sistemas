baaf3e14c664caadec2718a9e8985f5fc7b489bc
Luis Manzur <luismanzur91@outlook.com>
2022-03-13 13:55:30 -0400

    pagiacion

--- src/Components/allMovies.jsx
+++ src/Components/allMovies.jsx
@@ -4,26 +4,45 @@ import { MovieCard } from "./MovieCard";
 
 import { request } from '../utils/httpClient';
 import Nav from './nav';
+import { useNavigate } from 'react-router-dom';
 
 export function AllMovies(){
 
+    const [page, setPage] = useState(1)
 
     const [movies, setMovies]= useState([]);
 
+    const navigate = useNavigate()
 
     useEffect(() => {
-        request("/discover/movie")
+        request("/discover/movie?page=" + String(page))
         .then((data) => {
             setMovies(data.results);
         });
-    },[]);
+    },[page]);
 
+    
+
+    const handlePreviusPage = () => {
+        if (page > 1){
+            setPage(page - 1)
+        }
+    }
+    const handleNextPage = () => {
+        setPage(page + 1)
+        
+    }
 
     return (
-        <><Nav />
-        <ul className={styles.gridPeliculas}>
-            {movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
-
-        </ul></>
+        <>
+            <Nav />
+            <ul className={styles.gridPeliculas}>
+                {movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+            </ul>
+            <div className={styles.pagination}>
+                <button onClick={handlePreviusPage} className={styles.halfButton}>Prev</button>
+                <button onClick={handleNextPage} className={styles.halfButton}>Next</button>
+            </div>
+        </>
     );
 }
